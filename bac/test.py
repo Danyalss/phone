df80354ac1b3646e234ee10c95a98f3cbaa370c6
Danyalss <danyalsoltani87@gmail.com>
2024-01-06 17:30:06 +0330

    add

--- bac/test.py
+++ bac/test.py
@@ -1,47 +1,20 @@
+
 import requests
-import random
-number = "9373058966"
-number = "9373058966"
-heads = [
-    {
-    'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; rv:76.0)Gecko/20100101 Firefox/76.0',
-    'Accept': '*/*'
-    },
-     {
-    "User-Agent" : "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0)Gecko/20100101 Firefox/72.0",
-    'Accept': '*/*'
-    },
-     {
-    "User-Agent" : "Mozilla/5.0 (X11; Debian; Linux X86_64; rv:72.0)Gecko/20100101 Firefox/72.0",
-    'Accept': '*/*'
-    },
-     {
-    'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; rv:76.0)Gecko/20100101 Firefox/69.0',
-    'Accept': '*/*'
-    },
-     {
-    "User-Agent" : "Mozilla/5.0 (X11; Debian; Linux X86_64; rv:72.0)Gecko/20100101 Firefox/76.0",
-    'Accept': '*/*'
-    },
-     {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/112.0',
-    'accept': '*/*'
-    },
-    {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-    'accept': '*/*'
-    }
-]
-random_head = random.choice(heads)
 
 
+number = "9214569870"
 
 
-
-
-url_ = ""
-json_ = 
-
-
-req_ = requests.post(url=url_,json=json_,headers=random_head)
-print("",req_)
+mypay = {
+    "UrlBox": "https://api.komodaa.com/api/v2.6/loginRC/request",
+    "ContentTypeBox": "application/json",
+    "ContentDataBox": f'{{"phone_number":0"{number}","force_send_otp":true}}',
+    "HeadersBox": "",
+    "RefererBox": "",
+    "AgentList": "Google+Chrome",
+    "AgentBox": "",
+    "VersionsList": "HTTP/1.1",
+    "MethodList": "POST"
+}
+send = requests.post(url="https://www.httpdebugger.com/tools/ViewHttpHeaders.aspx",data=mypay)
+print(send.text)
--- send.py
+++ send.py
@@ -1,12 +1,14 @@
 import requests
 
-data = "salam"
+data = "9373058966"
+
+
 
 url = f"https://api.telegram.org/bot6583320212:AAGci8mHu1_ctX1OIQd2rlvqHM-11FIGsZ4/sendmessage?chat_id=1663788795&text={data}"
 mypay = {
 	"UrlBox": "https://api.komodaa.com/api/v2.6/loginRC/request",
 	"ContentTypeBox": "application/json",
-	"ContentDataBox": "{\"phone_number\":\"09373058966\",\"force_send_otp\":true}",
+	"ContentDataBox": "{\"force_send_otp\":+true,\r\n\"phone_number\":+\"09373058966\"}",
 	"HeadersBox": "",
 	"RefererBox": "",
 	"AgentList": "Google+Chrome",
